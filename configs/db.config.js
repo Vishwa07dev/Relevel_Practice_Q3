51f4df4ade2fd64e9581849f458c6cf70ed4dccf
Uday <budayteja009@gmail.com>
2022-05-17 19:11:56 +0530

    Added codes for story1

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DB_NAME : "relevel_hospital",
-    DB_URL : "127.0.0.1:27017//localhost/relevel_hospital"
+    DB_NAME : "get_fit",
+    DB_URL : "127.0.0.1:27017//localhost/get_fit"
 }
